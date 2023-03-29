2451d7c66c6081bc497abb3d922b054d19738292
rajnish523 <rajnish.kumar@firstperformance.com>
2023-03-29 11:36:46 +0530

    fixed

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ def scrape():
     chrome_path = "chromedriver"
     chrome_binary = "/usr/bin/google-chrome"
     chrome_options = Options()
+    chrome_options.binary_location = "/usr/bin/google-chrome"
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     service= Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(chrome_binary,service=service,options=chrome_options)
+    driver = webdriver.Chrome(service=service,options=chrome_options)
     username = "sankum@gmail.com"
     password = "navyug@123"
     driver.get('https://app.salesrobot.co/login')
